34824a05f037141b5f8117b0cd83ec623ce05484
0xHEXNUMBERS <0xhexnumbers@gmail.com>
2020-06-29 14:14:50 -0500

    add mcts acronym to doc.go
    this is purely here to help visibility on godoc.org and pkg.go.dev

--- doc.go
+++ doc.go
@@ -1,4 +1,5 @@
-//Package gmcts is a generic implementation of the Monte-Carlo Tree Search algorithm.
+//Package gmcts is a generic implementation of the
+//Monte-Carlo Tree Search (mcts) algorithm.
 //
 //This package attempts to save memory and time by caching states as to not
 //have duplicate nodes in the search tree. This optimization is efficient for
