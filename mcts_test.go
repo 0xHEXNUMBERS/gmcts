3715596c0586cb8226dff53f8dac6bf853c9cca8
0xHEXNUMBERS <0xhexnumbers@gmail.com>
2020-07-01 02:21:14 -0500

    move test utils to mcts_test.go

--- mcts_test.go
+++ mcts_test.go
@@ -8,6 +8,57 @@ import (
 	tictactoe "github.com/0xhexnumbers/go-tic-tac-toe"
 )
 
+func getPlayerID(ascii byte) Player {
+	if ascii == 'x' || ascii == 'X' {
+		return Player(0)
+	}
+	return Player(1)
+}
+
+type tttGame struct {
+	game tictactoe.Game
+}
+
+func (g tttGame) GetActions() []Action {
+	gameActions := g.game.GetActions()
+
+	actions := make([]Action, len(gameActions))
+
+	for i, a := range gameActions {
+		actions[i] = a
+	}
+
+	return actions
+}
+
+func (g tttGame) ApplyAction(a Action) (Game, error) {
+	move, ok := a.(tictactoe.Move)
+	if !ok {
+		return nil, fmt.Errorf("action not correct type")
+	}
+
+	game, err := g.game.ApplyAction(move)
+
+	return tttGame{game}, err
+}
+
+func (g tttGame) Player() Player {
+	return getPlayerID(g.game.Player())
+}
+
+func (g tttGame) IsTerminal() bool {
+	return g.game.IsTerminal()
+}
+
+func (g tttGame) Winners() []Player {
+	winner, _ := g.game.Winner()
+	if winner == '_' {
+		return []Player{Player(0), Player(1)}
+	}
+
+	return []Player{getPlayerID(winner)}
+}
+
 func TestTicTacToeDraw(t *testing.T) {
 	game := tttGame{tictactoe.NewGame()}
 	concurrentSearches := 1 //runtime.NumCPU()
